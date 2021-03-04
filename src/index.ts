297d5c641634b6d1a1c6fa6d8bf26749459972e0
Miguel Hernandez <miguelh72@outlook.com>
2021-03-03 21:41:04 -0500

    Fixed datastore loading fuctions. Added function to verify datastore integrity.

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,49 @@ import readline from 'readline';
 import chalk from 'chalk';
 import Nightmare, { IConstructorOptions } from 'nightmare';
 import { parse } from 'node-html-parser';
-import { skill, timeframe, Skill, Timeframe } from './types';
+import { skill, timeframe, Skill, Timeframe, getSkillName, KeyValueSet } from './types';
 
 interface IConstructorOptionsComplete extends IConstructorOptions {
     webPreferences?: any;
 }
 
+/**
+ * Error class for when server responds with critical error while attempting to load resource.
+ */
+export class ServersideException extends Error {
+    public code: string;
+
+    /**
+     * @param message The error's message to developer.
+     * @param code Code used to denote different errors.
+     */
+    constructor(message: string, code?: string) {
+        super(message);
+
+        // Maintains proper stack trace for where our error was thrown (only available on V8)
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, ServersideException);
+        }
+
+        this.name = 'ServersideException';
+        this.code = code || 'SSE';
+    }
+}
+
 const DATASTORE_FOLDER = 'datastore';
 const RATE_LIMIT_PERIOD = 1000; // ms
+const MAX_RETRIES = 3; // number of retries per URL
 const DAY_TIMELAPSE = 86400000; // ms
 const WEEK_TIMELAPSE = 604800000; // ms
-const FIRST_WEEK_2020 = 1609523133037; // ms
+const FIRST_WEEK_2020 = 1609529796557; // ms
 
 const highscoreEndpoint = 'http://secure.runescape.com/m=hiscore/ranking?category_type=0';
 
 const NUM_PROGRESS_BARS: number = 70;
 
-const warning: chalk.Chalk = chalk.bold.yellow;
-const result = chalk.bold.green;
+const error = chalk.bold.red;
+const warning = chalk.bold.yellow;
+const ok = chalk.bold.green;
 const progress = chalk.bold.cyan;
 
 /**
@@ -35,7 +60,7 @@ export function isInTesting(): boolean {
  * @param percentProgress Percent between 0 and 100, inclusively.
  * @throws RangeError
  */
-function printProgress(percentProgress: number) {
+function printProgress(percentProgress: number): void {
     const numProgressBars = Math.round((percentProgress / 100) * NUM_PROGRESS_BARS);
     if (percentProgress > 100 || percentProgress < 0) {
         throw new RangeError('Percentage must be a value between 0 and 100, inclusively.');
@@ -60,7 +85,7 @@ function getHighscoreURL(skill: Skill, timeframe: Timeframe, startTime: number, 
     return highscoreEndpoint + '&table=' + skill + '&time_filter=' + timeframe + '&date=' + startTime + '&page=' + pgNum;
 }
 
-function getHighscorePageset(skill: Skill, timeframe: Timeframe, startTime: number): string[] {
+function getHighscorePageSet(skill: Skill, timeframe: Timeframe, startTime: number): string[] {
     return [1, 51, 101, 201, 301, 401, 501, 1001, 2001, 3001].map(pgNum => getHighscoreURL(skill, timeframe, startTime, pgNum));
 }
 
@@ -75,7 +100,60 @@ function pause<T>(ms: number, ret?: T): Promise<T> {
     return new Promise(resolve => setTimeout(() => resolve((ret !== undefined) ? ret : (true as unknown) as T), ms));
 }
 
-async function loadPages(URLs: string[], subFolder?: string): Promise<Boolean> {
+async function loadSinglePage(URL: string, datastoreFolder?: string, rateLimitPeriod: number = RATE_LIMIT_PERIOD): Promise<boolean> {
+    const filename = getFilenameFromURL(URL);
+    const filepath = (datastoreFolder !== undefined) ? datastoreFolder + '/' + filename : filename;
+    if (fs.existsSync(filepath)) { return true; }
+
+    // TODO add list of pages without enough player, and dont query again
+
+    let result = { done: false, value: false };
+    for (let retry = 0; !result.done && retry <= MAX_RETRIES; retry++) {
+        const response: string = await new Nightmare({
+            show: false,
+            loadImages: false,
+            webPreferences: { images: false }
+        } as IConstructorOptionsComplete)
+            .goto(URL)
+            .wait('body')
+            .html(filepath, 'HTMLOnly')
+            .evaluate(() => document.documentElement.outerHTML)
+            .end()
+            .then((response: string) => response)
+            .catch((err: Error) => {
+                console.log(error(err));
+                return 'ElectronUnknownError \n' + err.name + '\n' + err.message;
+            });
+
+        if (!response.includes('ElectronUnknownError')) {
+            result.done = true;
+            if (response.includes('Sorry, there are currently no players to display')) {
+                // skip URL with not enough players to report
+                console.log(warning('\nThere were not enough players at URL:\n' + URL));
+                result.value = true;
+                if (fs.existsSync(filepath)) {
+                    fs.rmSync(filepath);
+                }
+            } else if (parse(response).querySelector('div.tableWrap') !== null) {
+                result.value = fs.existsSync(filepath);
+            }
+        } else {
+            console.log(error(response));
+        }
+        if (!result.value && fs.existsSync(filepath)) {
+            fs.rmSync(filepath);
+        }
+
+        await pause(rateLimitPeriod);
+    }
+
+    if (!result.done) {
+        console.log(error('\nServer responded with critical error at URL:\n' + URL));
+    }
+    return result.value;
+}
+
+async function loadPages(URLs: string[], subFolder?: string, rateLimitPeriod?: number): Promise<boolean> {
     if (!fs.existsSync(DATASTORE_FOLDER)) {
         fs.mkdirSync(DATASTORE_FOLDER);
     }
@@ -87,58 +165,79 @@ async function loadPages(URLs: string[], subFolder?: string): Promise<Boolean> {
         }
     }
 
-    async function loadPage(URL: string): Promise<boolean> {
-        const filename = datastoreFolder + '/' + getFilenameFromURL(URL);
-        if (fs.existsSync(filename)) { return true; }
-
-        let nightmare = new Nightmare({
-            show: false, waitTimeout: 5000, loadImages: false,
-            webPreferences: {
-                images: false,
-            }
-        } as IConstructorOptionsComplete);
-        return await nightmare
-            .goto(URL)
-            .wait('body')
-            .html(filename, 'HTMLOnly')
-            .evaluate(() => document.documentElement.outerHTML)
-            .end()
-            .then((response: string) => {
-                return (parse(response).querySelector('div.tableWrap') === null) ? false : fs.existsSync(filename)
-            })
-            .then(result => pause(RATE_LIMIT_PERIOD, result));
-    }
-
-    let successful = true;
+    const result: boolean[] = [];
     for (let i = 0; i < URLs.length; i++) {
         printProgress(Math.round((i / URLs.length) * 100));
         let URL = URLs[i];
-        successful = successful && (await loadPage(URL));
+        result.push(await loadSinglePage(URL, datastoreFolder, rateLimitPeriod));
     }
-    printProgress(100);
+    const successful = result.reduce((acc, res) => acc && res);
+    if (successful) { printProgress(100); }
 
     return successful;
 }
 
-(async () => {
-    const overallAlltimePages = getHighscorePageset(skill.overall, timeframe.alltime, new Date('01/01/2021').getTime());
-    const result = await loadPages(overallAlltimePages, 'test');
-    return { overallAlltimePages, result };
-})().then(res => console.log(res));
+async function loadSkill(sk: Skill, rateLimitPeriod?: number): Promise<boolean> {
+    if (sk === undefined || sk < 0 || sk > 28) { throw new RangeError('Skill must be a numerical value between 0 and 28, inclusively.'); }
 
+    const oneWeekBeforeToday = Date.now() - WEEK_TIMELAPSE;
+    const startOfWeeks = [FIRST_WEEK_2020];
+    while (startOfWeeks[startOfWeeks.length - 1] + WEEK_TIMELAPSE < oneWeekBeforeToday) {
+        startOfWeeks.push(startOfWeeks[startOfWeeks.length - 1] + WEEK_TIMELAPSE);
+    }
 
-//console.log(getFilenameFromURL('http://secure.runescape.com/m=hiscore/ranking?category_type=0&table=0&time_filter=0&date=1609477200000&page=1'));
+    const pages = startOfWeeks.reduce((acc: string[], weekStart: number) =>
+        acc.concat(getHighscorePageSet(sk, timeframe.weekly, weekStart)), []);
 
-// Request making using nightmare
-/*
-function getData(html: string) {
-    let doc = parse(html);
+    return loadPages(pages, getSkillName(sk), rateLimitPeriod);
+}
 
-    console.log({
-        title: doc.querySelector('title').text,
-        body: doc.querySelectorAll('div.tableWrap table')[1].querySelectorAll('tr').map((tr) => {
-            return tr.querySelectorAll('td')[1].text.trim();
-        }),
+async function update(sk: Skill, rateLimitPeriod?: number) {
+    console.log('Working on skill: ' + getSkillName(sk as Skill));
+    return loadSkill(sk, rateLimitPeriod).then(result => {
+        console.log({ result });
+        return result;
     });
 }
-*/
+
+async function updateAll(rateLimitPeriod?: number) {
+    for (const [_, skillNum] of Object.entries(skill)) {
+        await update(skillNum, rateLimitPeriod);
+    }
+}
+
+async function verifyDatabaseIntegrity() {
+    // Verify no invalid subdirectories
+    const directories = fs.readdirSync(DATASTORE_FOLDER);
+    for (let dir of directories) {
+        if ((skill as KeyValueSet<Skill>)[dir] === undefined) {
+            console.log(error(`Invalid subdirectory (not named after skill): ${dir}`));
+            return false;
+        }
+    }
+
+    // Get list of valid files
+    let filePaths: string[] = [];
+    for (let [skillName, _] of Object.entries(skill)) {
+        const directory = DATASTORE_FOLDER + '/' + skillName;
+        const fileNames = fs.readdirSync(directory).map(fileName => directory + '/' + fileName);
+        filePaths = filePaths.concat(fileNames);
+    }
+    const files: string[] = filePaths.map(filePath => fs.readFileSync(filePath, 'utf8'));
+
+    // Verify html pages
+    for (let index = 0; index < files.length; index++) {
+        const isProperFile = parse(files[index]).querySelector('div.tableWrap') !== null;
+
+        if (!isProperFile) {
+            console.log(error(`Incorrect file found at path:\n${filePaths[index]}`));
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//(async () => console.log(await verifyDatabaseIntegrity()))();
+
+updateAll(5000);
--- src/types.ts
+++ src/types.ts
@@ -1,34 +1,45 @@
 export type Skill = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28;
+export interface KeyValueSet<M> {
+    [key: string]: M
+}
 export const skill = {
     overall: 0 as Skill,
     attack: 1 as Skill,
     defense: 2 as Skill,
     strength: 3 as Skill,
-    Constitution: 4 as Skill,
-    Ranged: 5 as Skill,
-    Prayer: 6 as Skill,
-    Magic: 7 as Skill,
-    Cooking: 8 as Skill,
-    Woodcutting: 9 as Skill,
-    Fletching: 10 as Skill,
-    Fishing: 11 as Skill,
-    Firemaking: 12 as Skill,
-    Crafting: 13 as Skill,
-    Smithing: 14 as Skill,
-    Mining: 15 as Skill,
-    Herblore: 16 as Skill,
-    Agility: 17 as Skill,
-    Thieving: 18 as Skill,
-    Slayer: 19 as Skill,
-    Farming: 20 as Skill,
-    Runecrafting: 21 as Skill,
-    Hunter: 22 as Skill,
-    Construction: 23 as Skill,
-    Summoning: 24 as Skill,
-    Dungeoneering: 25 as Skill,
-    Divination: 26 as Skill,
-    Invention: 27 as Skill,
-    Archaeology: 28 as Skill,
+    constitution: 4 as Skill,
+    ranged: 5 as Skill,
+    prayer: 6 as Skill,
+    magic: 7 as Skill,
+    cooking: 8 as Skill,
+    woodcutting: 9 as Skill,
+    fletching: 10 as Skill,
+    fishing: 11 as Skill,
+    firemaking: 12 as Skill,
+    crafting: 13 as Skill,
+    smithing: 14 as Skill,
+    mining: 15 as Skill,
+    herblore: 16 as Skill,
+    agility: 17 as Skill,
+    thieving: 18 as Skill,
+    slayer: 19 as Skill,
+    farming: 20 as Skill,
+    runecrafting: 21 as Skill,
+    hunter: 22 as Skill,
+    construction: 23 as Skill,
+    summoning: 24 as Skill,
+    dungeoneering: 25 as Skill,
+    divination: 26 as Skill,
+    invention: 27 as Skill,
+    archaeology: 28 as Skill,
+}
+const skillNameMap: string[] = new Array<string>(28);
+for (const [skillName, skillNum] of Object.entries(skill)) {
+    skillNameMap[skillNum] = skillName;
+}
+export function getSkillName(sk: Skill) {
+    if (sk === undefined || sk < 0 || sk > 28) { throw new RangeError('Skill must be a numerical value between 0 and 28, inclusively.'); }
+    return skillNameMap[sk];
 }
 
 export type Timeframe = 0 | 1 | 2;
